57c523b4ab58f6a0e94ec9ef361a833d52a334b4
melinajiang <1029803819@qq.com>
2016-12-24 00:25:47 +0800

    实现显示图片大小自适应&测试图片调用
    1.在jlable中实现图片自适应jlable显示
    2.测试了主页与info页的跳转，与更具名字调用本地图片。

--- getPicPath.java
+++ getPicPath.java
@@ -4,21 +4,21 @@ import java.io.File;
 import java.util.HashMap;
 
 public class getPicPath {
-private HashMap< String, String> picPathMap;
-private HashMap<String, String> hotPicPathMap;
-private File [] hotPics;
+static private HashMap< String, String> picPathMap;
+//static private HashMap<String, String> hotPicPathMap;
+//private File [] hotPics;
 
 public getPicPath(HashMap< String, String> picPath) {
 	// TODO Auto-generated constructor stub
 	this.picPathMap = picPath;
-	hotPicPathMap = new HashMap<String, String>();
+	//hotPicPathMap = new HashMap<String, String>();
 }
 /*
 public HashMap< String, String> getPicPath() {
 	return picPath;
 }
 */
-public String getPicPath(String picName) {
+public String getOnePicPath(String picName) {
 	picName = picName+".PNG";
 	if (picPathMap.containsKey(picName)) {
 	String picPath = picPathMap.get(picName);
